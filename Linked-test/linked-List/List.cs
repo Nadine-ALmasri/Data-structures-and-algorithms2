adb60747bba9c7a3b4cdb9dd788f0e343e0044a4
user <user@DESKTOP-7AK0NU9>
2023-07-01 03:55:15 +0300

    done

---
+++ Linked-test/linked-List/List.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class List
+    {
+        public Node Head;
+        public Node Tail;
+        public List()
+        {
+            Head = Tail = null;
+        }
+
+
+        
+
+        public void Display()
+        {
+            Node Start = Head;
+            if (Start == null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            else
+            {
+                while (Start != null)
+                {
+                    Console.WriteLine(Start.Data);
+                    Start = Start.Next;
+                }
+            }
+        }
+        public void Insert(int value)
+        {
+            Node item = new Node(value);
+            if (Head == null)
+            {
+                Head = item;
+                Tail = item;
+            }
+            else
+            {
+                item.Next = Head;
+                Head = item;
+            }
+        }
+        public bool includes(int value)
+        {
+            Node current = Head;
+            int pos = 1;
+            while (current != null)
+            {
+                if (current.Data == value)
+                {
+                    Console.WriteLine($"THE VALUE {current.Data} WAS FOUND AT Position {pos}");
+                    return true;
+                }
+                else
+                {
+                    current = current.Next;
+                    pos++;
+                }
+                if (current == null)
+                {
+                    Console.WriteLine(" THE VALUE WAS NOT FOUND ");
+                }
+
+            }
+            return false;
+        }
+
+        public string ToString()
+        {
+            Node current = Head;
+            string result = ""; ;
+            while (current != null)
+            {
+
+                result += "{ " + current.Data + " } -> ";
+                current = current.Next;
+            }
+            result += "NULL";
+            return result;
+        }
+        public void InsertAfter(int Newvalue, int value)
+        {
+
+
+            Node current = Head;
+            while (current != null)
+            {
+                if (current.Data == value)
+                {
+                    Console.WriteLine($"INSERTING {Newvalue} AFTER {value}");
+                    Node New = new Node(Newvalue);
+                    New.Next = current.Next;
+                    current.Next = New;
+                    return;
+   
+                {
+                    current = current.Next;
+
+                }
+                throw new ArgumentException($"Value {value} not found in the linked list.");
+            }
+
+        }
+       }
+    }
+}
+    
+    
---
+++ Linked-test/linked-List/README.md
@@ -0,0 +1,32 @@
+# LinkedList
+This code provides an implementation of a singly linked list in C#. It includes a Node class that represents a node in the linked list, and a LinkedList class that manages the list operations.
+
+## Summary
+The linked list implementation provides the following functionality:
+
+- Creation of an empty linked list
+- Insertion of nodes at the head of the list
+- adds a new node with the given value to the end of the list
+
+- adds a new node with the given new value immediately before the first node that has the value specified
+
+- adds a new node with the given new value immediately after the first node that has the value specified
+
+## Description
+The Node class represents a node in the linked list. It has two properties: Value, which stores the value of the node, and Next, which points to the next node in the list.
+
+The List class manages the operations on the linked list. It has a Head property that points to the first node in the list. The class provides the following methods:
+
+- AddFirstList(int value) ,AddLastList(int value) : Inserts a new node with the given value at the head or the tail of the linked list.
+- 
+## Approach & Efficiency
+- Insertion: The Insert method adds a new node at the head of the linked list. It has a time complexity of O(1) since it always inserts at the beginning of the list.
+## Solution:
+The provided code implements the LinkedList class with the required methods to append nodes at the end, insert nodes before a specified node, and insert nodes after a specified node. The methods traverse the linked list and update the references accordingly to perform the desired insertions. The implementation ensures proper error handling, such as throwing exceptions when necessary (e.g., if a value is not found).
+
+
+
+
+
+
+
