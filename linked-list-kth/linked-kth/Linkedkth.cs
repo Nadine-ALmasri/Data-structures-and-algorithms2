d3298659f1787a2fe303a6695e5fb32cdfe2f3e0
user <user@DESKTOP-7AK0NU9>
2023-07-03 04:32:32 +0300

    almost done

---
+++ linked-list-kth/linked-kth/Linkedkth.cs
@@ -0,0 +1,27 @@
+﻿namespace LinkedList
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+       LinkedList I =new LinkedList();
+            I.append(5);
+            I.append(15);
+            I.AddFirstList(6);
+            I.Display();
+          
+            I.InsertAfter(3,6);
+            I.Display();
+       
+            I.Insertbefore(4,15);
+            I.Display();
+            Console.WriteLine("the Kth Method");
+            I.KthFromEnd(-1);
+
+
+          string y= I.ToString();
+            Console.WriteLine(y);
+         
+        }
+    }
+}
