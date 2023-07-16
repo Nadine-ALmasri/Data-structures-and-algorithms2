6117d4a108a9b187b06c12d58209dda11b186a48
user <user@DESKTOP-7AK0NU9>
2023-07-16 10:43:07 +0300

    cc11

---
+++ stack-queue-pseudo/stack-queue-pseudo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace stack_queue_pseudo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
