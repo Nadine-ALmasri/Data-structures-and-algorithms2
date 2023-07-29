e943c2c62d53ec54a4fec308c176e276f65aa14c
user <user@DESKTOP-7AK0NU9>
2023-07-29 23:51:04 +0300

    code added

---
+++ BinaryTree/BinarySearch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+  public class BinarySearch : BinaryTree
+    {
+        public void Add(int value)
+        {
+            Root = AddRecursive(Root, value);
+        }
+
+        private Node AddRecursive(Node current, int value)
+        {
+            if (current == null)
+            {
+                return new Node(value);
+            }
+
+            if (value < current.Value)
+            {
+                current.Left = AddRecursive(current.Left, value);
+            }
+            else if (value > current.Value)
+            {
+                current.Right = AddRecursive(current.Right, value);
+            }
+
+            return current;
+        }
+
+        public bool Contains(int value)
+        {
+            return ContainsRecursive(Root, value);
+        }
+
+        private bool ContainsRecursive(Node current, int value)
+        {
+            if (current == null)
+            {
+                return false;
+            }
+
+            if (current.Value == value)
+            {
+                return true;
+            }
+
+            if (value < current.Value)
+            {
+                return ContainsRecursive(current.Left, value);
+            }
+            else
+            {
+                return ContainsRecursive(current.Right, value);
+            }
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+    public BinaryTree()
+    {
+        Root = null;
+    }
+
+    // Depth-first traversal methods
+    public List<int> PreOrder()
+    {
+        List<int> result = new List<int>();
+        PreOrderTraversal(Root, result);
+        return result;
+    }
+
+    private void PreOrderTraversal(Node node, List<int> result)
+    {
+        if (node == null) return;
+        result.Add(node.Value);
+        PreOrderTraversal(node.Left, result);
+        PreOrderTraversal(node.Right, result);
+    }
+
+    public List<int> InOrder()
+    {
+        List<int> result = new List<int>();
+        InOrderTraversal(Root, result);
+        return result;
+    }
+
+    private void InOrderTraversal(Node node, List<int> result)
+    {
+        if (node == null) return;
+        InOrderTraversal(node.Left, result);
+        result.Add(node.Value);
+        InOrderTraversal(node.Right, result);
+    }
+
+    public List<int> PostOrder()
+    {
+        List<int> result = new List<int>();
+        PostOrderTraversal(Root, result);
+        return result;
+    }
+
+    private void PostOrderTraversal(Node node, List<int> result)
+    {
+        if (node == null) return;
+        PostOrderTraversal(node.Left, result);
+        PostOrderTraversal(node.Right, result);
+        result.Add(node.Value);
+    }
+}
+
+}
