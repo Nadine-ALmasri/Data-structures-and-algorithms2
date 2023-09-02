de2d0db97e472ec6b71d82cbd1099a846a104bdb
user <user@DESKTOP-7AK0NU9>
2023-09-02 20:55:26 +0300

    cc27 done

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 8, 4, 23, 42, 16, 15 };
+            Console.WriteLine("Giving Array");
+          
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sorted Array");
+            MargeSorts.MargeSorting(array);
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
