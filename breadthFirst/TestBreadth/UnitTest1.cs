d8ebd805895bf87bdb02ca39a5d37e716c50a334
user <user@DESKTOP-7AK0NU9>
2023-10-07 05:09:37 +0300

    cc36

---
+++ breadthFirst/TestBreadth/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestBreadth
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ breadthFirst/breadth-first/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breadth_first
+{
+    internal class Node
+    {
+        public string Value { get; }
+        public List<Node> Neighbors { get; }
+
+        public Node(string value)
+        {
+            Value = value;
+            Neighbors = new List<Node>();
+        }
+    }
+}
